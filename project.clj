4adc1d9c9f006fd54ce21f262413baf2c8a7eb32
Earl J. Wagner <ewagner@awarenesys.com>
2013-02-05 16:58:19 -0800

    switched to local version of sspace, maven verson of jama

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject clj-sspace "0.1.0-SNAPSHOT"
+(defproject org.clojars.earljwagner/clj-sspace "0.1.0-SNAPSHOT"
   :description "Clojure wrapper for S-Space Java library"
   :url "http://github.com/earljwagner/clj-sspace"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
 
-                 [sspace "2.0.3"]
-                 [jama "1.0.3"]
+                 [edu.ucla.sspace "2.0.3-1"]
+                 [gov.nist.math/jama "1.0.2"]
 
                  [stout "0.1.0"]
                  ])
